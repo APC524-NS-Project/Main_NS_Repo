10a5b553d7ec57599dbc466668bcc610b7cc3d17
agilman0803 <agilman@princeton.edu>
2020-01-12 15:39:36 -0500

    Implement OperatorND class; no tests

--- src/operator_1d.py
+++ src/operator_1d.py
@@ -13,9 +13,20 @@ class Operator1D():
     #  \param s_list A list of stencil index points
     #  \param deg The degree of the desired derivative
     def __init__(self, s_list, deg):
+        
+        ## \var stncl A stencil object
         self.stncl = stencil.Stencil(s_list)
+
+        ## \var deg The derivative degree
         self.deg = deg
+
+        ## \var accuracy Accuracy of the approximation, which is
+        #  equal to the number of stencil points minus the
+        #  derivative degree
         self.accuracy = self.stncl.N - self.deg
         assert self.accuracy > 0, '''Improper 1D operator formation: 
                                   increase number of stencil points!'''
+
+        ## \var weights The appropriate weighting coefficients for
+        #  the finite difference approximation
         self.weights = self.stncl.get_weights(deg)
