ee2fc2e1a2eac736cf455252eaa8048862580c43
jamesvr <j.v.roggeveen@gmail.com>
2020-01-13 00:01:05 -0500

    added documentation for OperatorMatrix

--- src/OperatorMatrix.py
+++ src/OperatorMatrix.py
@@ -1,10 +1,18 @@
 import numpy as np
 
+## OperatorMatrix
+# A Finite Difference Linear operator expressed as a matrix corresponding to the relevant combinations of values in a grid
 class OperatorMatrix():
+	## Constructor
+	# initialize the operator matrix to be a numpy array full of zeros
+	#
+	# @param N dimensions of the matrix operator
 	def __init__(self,N):
 		self.shape = (N,N)
 		self.array = np.zeros(self.shape)
 
+	## __getattr__
+	# Recast attribute calls to calls on the underlying array
 	def __getattr__(self, name):
 		try:
 			return getattr(self.array, name)
