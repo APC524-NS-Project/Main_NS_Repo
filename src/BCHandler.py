78f05d136766da72632c2c12d6b3f674e2fc124e
KBergst <kendra.bergstedt@gmail.com>
2020-01-14 10:50:50 -0500

    Removed broken code from this branch
    work still exists on BChandler_class_implementation

--- src/BCHandler.py
+++
@@ -1,7 +0,0 @@
-## @file
-#
-# file containing the abstract base class for applying the PDE problem's boundary conditions.
-#
-# Uses the 'abc' module to utilize pythonic abstract base classes
-
-
--- tests/test_BCHandler.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 10 10:19:15 2020
-
-@author: kbergste
-"""
-
