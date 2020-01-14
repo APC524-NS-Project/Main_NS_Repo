7234a64bedd3c7209c615d882c150a53fed9ed0f
KBergst <kendra.bergstedt@gmail.com>
2020-01-14 11:52:23 -0500

    Updated initializer and main
    Updated initializer and main to instantiate GridSpec, GridScalar, and DirichletHand objects. Moved all user-specified (hard-coded) information to initializer.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+import numpy as np
+from src import grid
 from src import static_bcs
 from src import dirichlet_hand
 from src import logger
@@ -8,24 +9,15 @@ from src import spatial_driver
 from src import driver
 import initializer
 
-t_start = 0.0 # start time
-t_end = 10.0 # end time
-dt = 0.01 # time step size
-
-grid_T = None # initial grid of temperature values
+gspec = grid.CartesianGridSpec(initializer.coords)
+grid_u = grid.GridScalar(gspec,initializer.u)  # initial grid of temperature values
 
 #set each of the boundaries (using all dirichlet zero for now)
 BCs = []
-BCs.append(static_bcs.Dirichlet(0,'l',0))
-BCs.append(static_bcs.Dirichlet(0,'r',0))
-BCs.append(static_bcs.Dirichlet(1,'l',0))
-BCs.append(static_bcs.Dirichlet(1,'r',0))
-#set each of the boundaries (using all dirichlet zero for now)
-BCs = []
-BCs.append(static_bcs.Dirichlet(0,'l',None))
-BCs.append(static_bcs.Dirichlet(0,'r',None))
-BCs.append(static_bcs.Dirichlet(1,'l',None))
-BCs.append(static_bcs.Dirichlet(1,'r',None))
+BCs.append(static_bcs.Dirichlet(0,'l',np.zeros(grid_u.shape[0])))
+BCs.append(static_bcs.Dirichlet(0,'r',np.zeros(grid_u.shape[0])))
+BCs.append(static_bcs.Dirichlet(1,'l',np.zeros(grid_u.shape[0])))
+BCs.append(static_bcs.Dirichlet(1,'r',np.zeros(grid_u.shape[0])))
 
 bound_handlr = dirichlet_hand.DirichletHand(BCs)
 
@@ -43,4 +35,4 @@ space_drive = spatial_driver.SpatialDriver(bound_handlr,
 
 drive = driver.Driver(space_drive, time_stpr)
 
-drive.full_solve(t_start, t_end, dt, grid_T)
+drive.full_solve(initializer.t_start, initializer.t_end, initializer.dt, grid_u)
