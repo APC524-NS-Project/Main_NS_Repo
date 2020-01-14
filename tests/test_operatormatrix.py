a1d032d1f0a9361930f9809ceecbb3d9547d6f76
jamesvr <j.v.roggeveen@gmail.com>
2020-01-14 03:23:05 -0500

    added __add__ functionality to operator matrix

---
+++ tests/test_operatormatrix.py
@@ -0,0 +1,17 @@
+import unittest
+import numpy as np
+from src import operatormatrix
+
+class testOpMat(unittest.TestCase):
+	def test_add(self):
+		op1 = operatormatrix.OperatorMatrix(3)
+		op2 = operatormatrix.OperatorMatrix(3)
+		op1[0,0] = 1
+		op2[0,1] = 2
+
+		op3 = operatormatrix.OperatorMatrix(3)
+		op3[0,0:2] = [1,2]
+
+		op4 = op1 + op2
+
+		np.testing.assert_array_equal(op4.array,op3.array)
