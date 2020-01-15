0d1b205f93ddcea04958c17d4199bda32826305a
jamesvr <j.v.roggeveen@gmail.com>
2020-01-14 21:44:19 -0500

    added plot2d and tests

--- src/plot_end.py
+++ src/plot_end.py
@@ -1,6 +1,8 @@
 from . import visualizer
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import BoundaryNorm
+from matplotlib.ticker import MaxNLocator
 
 class PlotEnd(visualizer.Visualizer):
 	def __init__(self,outloc):
@@ -11,14 +13,52 @@ class PlotEnd(visualizer.Visualizer):
 		suffix = ".png"
 		last_frame = log.get_frame(-1)
 
+		initial_frame = log.get_frame(0)
+
 		if last_frame[1].spec.ndim != 1:
 			raise IndexError("Attempting to do a 1d plot of a multi dimensional grid")
 
 		x = last_frame[1].spec.coords
 		x = np.array(x)
 
-		plt.plot(x,last_frame[1].grid)
+		plt.figure()
+		plt.subplot(211)
+		plt.plot(x,initial_frame[1].grid,**kwargs)
+		plt.subplot(212)
+		plt.plot(x,last_frame[1].grid,**kwargs)
 		plt.savefig(self.outloc+fext+suffix)
 		plt.close(fig='all')
 
+
+	def plot2d(self,log,name="",**kwargs):
+		fext = "2d_end_"+name
+		suffix = ".png"
+		last_frame = log.get_frame(-1)
+
+		initial_frame = log.get_frame(0)
+
+		if last_frame[1].spec.ndim != 2:
+			raise IndexError("Attempting to do a 2d plot of a multi dimensional grid")
+
+		x,y=log.get_frame(0)[1].spec.coords #sets up coordinates for plot using GridSpec coordinate tuple
+		xmesh,ymesh=np.meshgrid(x,y,indexing='ij')
+		levels = MaxNLocator(nbins=15).tick_values(initial_frame[1].grid.min(), initial_frame[1].grid.max())
+
+		cmap = plt.get_cmap('PiYG')
+		norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
+
+		fig, (ax0, ax1) = plt.subplots(nrows=2)
+
+		cf0 = ax0.contourf(x,y,initial_frame[1].grid, levels=levels,cmap=cmap,**kwargs)
+		fig.colorbar(cf0, ax=ax0)
+		ax1.set_title('Initial State')
+
+		cf1 = ax1.contourf(x,y,last_frame[1].grid, levels=levels,cmap=cmap,**kwargs)
+		fig.colorbar(cf1, ax=ax1)
+		ax1.set_title('Final State')
+
+		fig.tight_layout()
+
+		plt.savefig(self.outloc+fext+suffix)
+		plt.close(fig='all')
 
--- tests/test_plotend.py
+++ tests/test_plotend.py
@@ -6,22 +6,39 @@ import numpy as np
 class testPlotEnd(unittest.TestCase):
 	def setUp(self):
 		self.plot = plot_end.PlotEnd(r"tests/test_outs/")
-		self.log = Logger()
+		self.log1d = Logger('1d')
+		self.log2d = Logger('2d')
 
 	def test_plot1d(self):
-		self.plot.plot1d(self.log,"test1")
+		self.plot.plot1d(self.log1d,"test")
+
+	def test_plot2d(self):
+		self.plot.plot2d(self.log2d,"test")
 
 
 class Logger():
-	def __init__(self):
-		self.grid = Grid()
+	def __init__(self,gridtype):
+		if gridtype == '1d':
+			self.grid = Grid1D()
+		elif gridtype == '2d':
+			self.grid = Grid2D()
 
 	def get_frame(self,i):
 		return [0,self.grid]
 
-class Grid():
+class Grid1D():
 	def __init__(self):
 		self.spec = mock.Mock()
 		self.spec.coords = (0,1,2,3,4,5)
 		self.spec.ndim = 1
 		self.grid = np.array([5,4,3,2,1,0])
+
+class Grid2D():
+	def __init__(self):
+		self.spec = mock.Mock()
+		self.spec.coords = ((0,1,2,3,4,5),(0,1,2,3,4,5))
+		self.spec.ndim = 2
+		self.grid = np.ones((6,6))
+		self.grid[1:5,1:5] = 1
+		self.grid[2:4,2:4] = 2
+		self.grid[3,3] = 3
